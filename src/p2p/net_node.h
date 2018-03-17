a5fa37280fd35b00a91d3f4e3964d670fc9ab04a
techqc <37153171+techqc@users.noreply.github.com>
2018-03-16 21:59:21 -0400

    add seed

--- src/p2p/net_node.h
+++ src/p2p/net_node.h
@@ -127,11 +127,11 @@ namespace nodetool
     virtual std::map<std::string, time_t> get_blocked_hosts() { CRITICAL_REGION_LOCAL(m_blocked_hosts_lock); return m_blocked_hosts; }
   private:
     const std::vector<std::string> m_seed_nodes_list =
-    { "seeds.moneroseeds.se"
-    , "seeds.moneroseeds.ae.org"
-    , "seeds.moneroseeds.ch"
-    , "seeds.moneroseeds.li"
-    };
+    { "192.168.0.8:18667"
+    , "192.168.0.8:18666"
+
+
+};
 
     bool islimitup=false;
     bool islimitdown=false;
